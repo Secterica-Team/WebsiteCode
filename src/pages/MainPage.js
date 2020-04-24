0bad729717882cddab9de707f99f824af8763749
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-24 22:11:02 +0300

    Fixed map position, added markers

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,16 +3,61 @@ import {Map, Marker, Popup, TileLayer} from "react-leaflet";
 import {Icon} from "leaflet";
 import "./MainPage.css";
 
+
 class MainPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            locations: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://heysmellproject-env.eba-uctmjbw3.us-east-2.elasticbeanstalk.com/air-quality/locations")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        locations: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
     render() {
-        return (
-            <Map center={[49.839684, 24.029716]} zoom={13}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </Map>
-        );
+        const {error, isLoaded, locations} = this.state;
+        if (error) {
+            return <div>Oops..something went wrong: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <React.Fragment>
+                <Map center={[49.839684, 24.029716]} zoom={13}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+
+                    {locations.map(location => (
+                        <Marker
+                            key={location.id}
+                            position={[location.latitude, location.longitude]}
+                        />
+                    ))}
+                </Map>
+                </React.Fragment>
+            );
+        }
     }
 }
 
