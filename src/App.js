474e69b19dc0e68be12b974271b7d032b1c55d89
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-26 11:52:14 +0300

    Added redirection from page to page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import Location from './pages/Location';
+import MainNavigation from './components/Navigation/MainNavigation';
+import LocationContext from './context/location-context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        currentLocation: null,
+        locationId: null
+    };
+
+    putCurrentLocation = (currentLocation, locationId) => {
+        this.setState({currentLocation: currentLocation, locationId: locationId});
+    };
+
+    render() {
+        return (
+            <BrowserRouter>
+                <React.Fragment>
+                    <LocationContext.Provider
+                        value={{
+                            currentLocation: this.state.currentLocation,
+                            locationId: this.state.locationId,
+                            putCurrentLocation: this.putCurrentLocation
+                        }}>
+                        <MainNavigation/>
+                        <main className="main-content">
+                            <Switch>
+                                <Redirect from="/" to="/main" exact/>
+                                <Route path="/main" component={MainPage}/>
+                                <Route path="/location" component={Location}/>
+                            </Switch>
+                        </main>
+                    </LocationContext.Provider>
+                </React.Fragment>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
