e080285509c91d08f476871de217db35c57cb70c
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-26 11:55:06 +0300

    Added files with location meta data page

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Location extends Component {
     render() {
         return(
+            <div>
             <h1>Location</h1>
+            </div>
+
         );
     }
 }
