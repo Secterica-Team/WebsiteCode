2c37054b0d8154741591c75e24e3d50a0b34f300
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-26 13:46:36 +0300

    Added logic to retrieve data from local storage

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import "./Location.css";
+import  LocationContext from '../context/location-context'
 
 class Location extends Component {
+
+    static contextType = LocationContext;
+
+    componentDidMount() {
+        const currentLocationToDisplay = localStorage.getItem('currentLocation');
+        const currentLocationToDisplayId = localStorage.getItem('currentLocationId');
+        this.context.putCurrentLocation(currentLocationToDisplay, currentLocationToDisplayId);
+    }
+
     render() {
         return(
-            <div>
+            <div className="my_current_location">
             <h1>Location</h1>
             </div>
 
