d5cff3539675b2deb19095069a928a2206672412
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-05-01 00:36:08 +0300

    Added map to the location page

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import "./Location.css";
 import LocationContext from '../context/location-context';
+import {Map, Marker, TileLayer} from "react-leaflet";
+import {Icon} from "leaflet";
 
+
+const myMarker = new Icon({
+    iconUrl: require("../AQI_Warning.svg"),
+    iconSize: [80, 85]
+});
 
 class Location extends Component {
 
@@ -20,7 +27,10 @@ class Location extends Component {
     componentDidMount() {
         const currentLocationToDisplay = localStorage.getItem('currentLocation');
         const currentLocationToDisplayId = localStorage.getItem('currentLocationId');
-        this.context.putCurrentLocation(currentLocationToDisplay, currentLocationToDisplayId);
+        const currentLocationToDisplayName = localStorage.getItem('currentLocationName');
+        const currentLocationToDisplayLatitude = localStorage.getItem('currentLocationLatitude');
+        const currentLocationToDisplayLongitude = localStorage.getItem('currentLocationLongitude');
+        this.context.putCurrentLocation(currentLocationToDisplay, currentLocationToDisplayId, currentLocationToDisplayName, currentLocationToDisplayLatitude, currentLocationToDisplayLongitude);
         fetch(`http://heysmellproject-env.eba-uctmjbw3.us-east-2.elasticbeanstalk.com/air-quality/last_day?location=${encodeURIComponent(currentLocationToDisplayId)}`)
             .then(res => res.json())
             .then(
@@ -30,6 +40,15 @@ class Location extends Component {
                         allDayData: result,
                         currentDayMetaData: result[result.length - 1]
                     });
+                    console.log(result);
+                    localStorage.setItem('currentDayMetaData', this.state.currentDayMetaData);
+                    this.context.putCurrentMetadata(this.state.currentDayMetaData.co,
+                                                    this.state.currentDayMetaData.co2,
+                                                    this.state.currentDayMetaData.dus,
+                                                    this.state.currentDayMetaData.hum,
+                                                    this.state.currentDayMetaData.tmp,
+                                                    this.state.currentDayMetaData.smk,
+                                                    this.state.currentDayMetaData.lpg);
                 },
                 (error) => {
                     this.setState({
@@ -48,10 +67,22 @@ class Location extends Component {
             return <div>Loading...</div>;
         } else {
             return (
-                <div className="my_current_location">
-                    <h1>Location</h1>
-                </div>
-
+                <React.Fragment>
+                    <div className="my_current_location">
+                        <Map className="my_map" center={[this.context.locationLatitude, this.context.locationLongitude]}
+                             zoom={13}>
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            />
+                            <Marker
+                                key={this.context.locationId}
+                                position={[this.context.locationLatitude, this.context.locationLongitude]}
+                                icon={myMarker}
+                            />
+                        </Map>
+                    </div>
+                </React.Fragment>
             );
         }
     }
