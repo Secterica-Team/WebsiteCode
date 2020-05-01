2d2316b63758efc6a1cb00534a80856970927b0e
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-05-02 01:07:35 +0300

    Created window with metadata

--- src/pages/Metadata.js
+++ src/pages/Metadata.js
@@ -1,24 +1,29 @@
 import React, {Component} from 'react';
 import LocationContext from '../context/location-context';
+import "./Metadata.css";
+import "./Rechart";
+import Recharts from "./Rechart";
 
 class MetadataWindow extends Component {
     static contextType = LocationContext;
 
     componentDidMount() {
-        const co = this.context.currentDayMetadataCO;
-        const co2 = this.context.currentDayMetadataCO2;
-        const dust = this.context.currentDayMetadataDust;
-        const humidity = this.context.currentDayMetadataHumidity;
-        const temperature = this.context.currentDayMetadataTemperature;
-        const smoke = this.context.currentDayMetadataSmoke;
-        const lpq = this.context.currentDayMetadataLPQ;
 
     }
 
     render() {
             return (
                 <React.Fragment>
-
+                    <div className="my_window">
+                        <h1>Last updated:<span className="tab">{this.context.currentDayMetadataTime}</span></h1>
+                        <h1>Temperature<span className="tab1">{this.context.currentDayMetadataTemperature}</span></h1>
+                        <h1>Humidity<span className="tab2">{this.context.currentDayMetadataHumidity}</span></h1>
+                        <h1>Dust<span className="tab3">{this.context.currentDayMetadataDust}</span></h1>
+                        <h1>Smoke<span className="tab4">{this.context.currentDayMetadataSmoke}</span></h1>
+                        <h1>CO<span className="tab5">{this.context.currentDayMetadataCO}</span></h1>
+                        <h1>CO<sub>2</sub> <span className="tab6">{this.context.currentDayMetadataCO2}</span></h1>
+                        <h1> LPG <span className="tab7">{this.context.currentDayMetadataLPQ}</span></h1>
+                    </div>
                 </React.Fragment>
             );
         }
