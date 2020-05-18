e52dc4803a7e4aa10f4b15596c5aec2344c889d1
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-05-18 12:52:37 +0300

    Updated rechart with new features

--- src/pages/Rechart.js
+++ src/pages/Rechart.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList } from 'recharts'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList,Legend } from 'recharts'
 import "./Rechart.css"
 
 const colors = {
     area: '#8aacc0',
     stroke: "#8aacc0",
-    grid_stroke: "#8aacc0"
+    grid_stroke: "rgba(138,172,192,0.22)"
 };
 
 class Recharts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          showChart: this.props.showChart
+        };
+    }
     render () {
         return (
             <div className="chart_block" style={{maxWidth:400, maxHeight: 300}}>
-            <ResponsiveContainer className="container" height={250} width={600}>
+                {this.state.showChart && <ResponsiveContainer className="container" height={250} width={600}>
                 <AreaChart data={this.props.data}
                            margin={{top:25, right:30, bottom:25, left:0}}
                            width={200}
@@ -27,10 +33,10 @@ class Recharts extends Component {
                     >
                         {/*<LabelList dataKey="y" position = "top" offset={10}/>*/}
                     </Area>
-                    {/*<CartesianGrid stroke={colors.grid_stroke} strokeDashArray="5 5"/>*/}
-                    <Tooltip/>
+                    <CartesianGrid stroke={colors.grid_stroke} strokeDasharray="8 8"/>
+                    <Tooltip />
                 </AreaChart>
-            </ResponsiveContainer>
+            </ResponsiveContainer>}
             </div>
         );
     }
